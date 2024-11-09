1e4aab9066595814bacd434f57e8b9e60f9efb89
blackzeshi <blackzeshi@mail.com>
2024-11-09 18:44:14 +0500

    FUUUUCK

--- flake.nix
+++ flake.nix
@@ -7,32 +7,49 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim = {
-      url = "github:nix-community/nixvim";
+      # url = "github:nix-community/nixvim";
       # url = "github:elythh/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:dc-tec/nixvim";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, nixvim, ... }@inputs: 
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      home-manager,
+      nixvim,
+      ...
+    }@inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       pkgs-stable = nixpkgs-stable.legacyPackages.${system};
-      nixvim = nixvim.legacyPackages.${system};
-    in {
+    in
+    # nixvim = nixvim.legacyPackages.${system};
+    {
       nixosConfigurations = {
         nixeshi = lib.nixosSystem {
           inherit system;
-          modules = [ 
-            ./configuration.nix 
+          modules = [
+            ./configuration.nix
+            {
+              environment.systemPackages = [
+                inputs.zen-browser.packages."${system}".default
+                inputs.nixvim.packages.${system}.default
+              ];
+            }
             # inputs.nixvim.nixosModules.nixvim
           ];
           specialArgs = {
             inherit pkgs-stable;
           };
+        };
       };
-    };
       # nvim = nixvim.makeNixvimWithModule {
       #   inherit pkgs;
       #   module = ./nixvimconfig;
